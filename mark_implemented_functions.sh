96f9e897f9fcfde7732dc4ed0a844de881532311
jonas <jonasmuehlmann@protonmail.com>
2020-12-26 18:20:22 +0100

    fix: Correct path after broken refactor

--- mark_implemented_functions.sh
+++ mark_implemented_functions.sh
@@ -3,7 +3,7 @@
 # find all lines with a checkbox '- [ ]' and get the line without the '- [ ]' (remove first 6 chars)
 grep -e '-\ \[ \]' README.md | cut -c6- | while read function; do
 	# check if function defined in source file
-	if    grep -E "^def ${function}"             src/std.py 1> /dev/null; then
+	if    grep -E "^def ${function}" std.py 1> /dev/null; then
 		# check checkbox
 		sed       -i "s/^-\ \[\ \] ${function}$/- [x] ${function}/g" README.md
 	fi
