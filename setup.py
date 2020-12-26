4c81752a2690b7971ba6c0a2d4ff45d8a3ae9927
jonas <jonasmuehlmann@protonmail.com>
2020-12-26 16:58:03 +0100

    build: Add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/bin/env python3
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as readme:
+    long_description = readme.read()
+
+__version__ = "0.0.0"
+
+setuptools.setup(
+    name="pyaoi",
+    version=__version__,
+    author="Jonas Muehlmann",
+    author_email="jonasmuehlmann@protonmail.com",
+    description="A collection of useful functions operating on iterables",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JonasMuehlmann/pyaoi",
+    py_modules=["pyaoi"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires='>=3.6',
+)
