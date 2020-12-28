c788046bfa057609c58d6e39d1e0ba8b11cc23bb
jonas <jonasmuehlmann@protonmail.com>
2020-12-28 14:35:02 +0100

    style: reformat, refactor, do renames
    -Refactored docstrings to conform to the google docstring convention
    -Renamed parameters and their type hints to correct typing errors
    -Fixed spelling errors
    
    BREAKING CHANGE: Parameter n of functions for_each_n() has been
    renamed to num_elements
    
    BREAKING CHANGE: Parameters iterable1 and iterable2 of function
    mismatch() has been renamed to sequence1 and sequence2
    
    BREAKING CHANGE: Parameter collection of function adjacent_find() has
    been renamed to sequence
    
    BREAKING CHANGE: Parameters collection_super and collection_sub of
    function search() have been renamed to sequence_super and sequence_sub
    
    BREAKING CHANGE: Parameter sequence of function search_n() has been
    renamed to sequence
    
    BREAKING CHANGE: Parameter n of function search_n() has been renamed
    to num_elements

--- pyaoi.py
+++ pyaoi.py
@@ -1,4 +1,5 @@
-"""A collection of functions operating on iterables"""
+#!/usr/bin/env python3
+"""A collection of functions operating on iterables."""
 #  This file is part of pyaoi.
 #  Copyright (C) 2020 Jonas Muehlmann
 #
@@ -28,7 +29,8 @@
 #      You should have received a copy of the GNU General Public License
 #      along with pyaoi.  If not, see <https://www.gnu.org/licenses/>.
 #
-#!/usr/bin/env python3
+
+
 import itertools
 import operator
 from typing import (
@@ -52,8 +54,7 @@ UnaryFunction = Callable[[Any], None]
 
 
 def all_of(iterable: Iterable, unary_predicate: UnaryPredicate) -> bool:
-    """
-    Check if an unary predicate returns True for all elements in the iterable, or if the iterable is empty
+    """Check if an unary predicate returns True for all elements in the iterable, or if the iterable is empty.
 
     Args:
         iterable: An iterable to apply the unary_predicate to
@@ -66,8 +67,7 @@ def all_of(iterable: Iterable, unary_predicate: UnaryPredicate) -> bool:
 
 
 def any_of(iterable: Iterable, unary_predicate: UnaryPredicate) -> bool:
-    """
-    Check if an unary predicate returns True for any elements in the iterable, or if the iterable is empty
+    """Check if an unary predicate returns True for any elements in the iterable, or if the iterable is empty.
 
     Args:
         iterable: An iterable to apply the unary_predicate to
@@ -80,8 +80,7 @@ def any_of(iterable: Iterable, unary_predicate: UnaryPredicate) -> bool:
 
 
 def none_of(iterable: Iterable, unary_predicate: UnaryPredicate) -> bool:
-    """
-    Check if an unary predicate returns False for any elements in the iterable, or if the iterable is empty
+    """Check if an unary predicate returns False for any elements in the iterable, or if the iterable is empty.
 
     Args:
         iterable: An iterable to apply the unary_predicate to
@@ -98,8 +97,7 @@ def for_each(
     unary_function: UnaryFunction,
     readonly: bool = True,
 ) -> None:
-    """
-    Apply an unary function to each element in the mutable_sequence, modifying the elements
+    """Apply an unary function to each element in the mutable_sequence, modifying the elements.
 
     Args:
         mutable_sequence: A mutable iterable to apply the unary_function to
@@ -116,28 +114,28 @@ def for_each(
 def for_each_n(
     mutable_sequence: MutableSequence,
     unary_function: UnaryFunction,
-    n: int,
+    num_elements: int,  # noqa: VNE001,C0103
     readonly: bool = True,
 ) -> None:
-    """
-    Apply an unary function to the first n elements in the mutable_sequence, modifying the elements
+    """Apply an unary function to the first num_elements elements in the mutable_sequence, modifying the elements.
 
     Args:
         mutable_sequence: A mutable iterable to apply the unary_function to
         unary_function: An unary function to apply to each element in the mutable_sequence
-        n: A number indicating the number of elements (counted from the start) to apply the unary_function to
+        num_elements: A number indicating the number of elements (counted from the start) to apply the unary_function to
         readonly: indicates whether the mutable_sequence is intended to be read or modified,defaults to True
     """
     if readonly:
-        for element in mutable_sequence[:n]:
+        for element in mutable_sequence[:num_elements]:
             unary_function(element)
     else:
-        mutable_sequence[:n] = list(map(unary_function, mutable_sequence[:n]))
+        mutable_sequence[:num_elements] = list(
+            map(unary_function, mutable_sequence[:num_elements])
+        )
 
 
 def count(sequence: Sequence, target: Any) -> int:
-    """
-    Count how often target appears in sequence
+    """Count how often target appears in sequence.
 
     Args:
         sequence: An iterable in which occurrences of target are counted
@@ -150,8 +148,7 @@ def count(sequence: Sequence, target: Any) -> int:
 
 
 def count_if(iterable: Iterable, unary_predicate: UnaryPredicate) -> int:
-    """
-    Count for how many elements in a iterable an unary predicate returns True
+    """Count for how many elements in a iterable an unary predicate returns True.
 
     Args:
         iterable: An iterable for which to count for how many elements unary_predicate returns True
@@ -161,12 +158,10 @@ def count_if(iterable: Iterable, unary_predicate: UnaryPredicate) -> int:
         For how many items unary predicate returned True
     """
     return sum(map(unary_predicate, iterable))
-    # return reduce(lambda x, y: x + unary_predicate(y), iterable, 0)
 
 
 def count_if_not(iterable: Iterable, unary_predicate: UnaryPredicate) -> int:
-    """
-    Count for how many elements in a iterable an unary predicate returns False
+    """Count for how many elements in a iterable an unary predicate returns False.
 
     Args:
         iterable: An iterable for which to count for how many elements unary_predicate returns False
@@ -183,54 +178,52 @@ def count_if_not(iterable: Iterable, unary_predicate: UnaryPredicate) -> int:
 
 
 def mismatch(
-    iterable1: Iterable,
-    iterable2: Iterable,
+    sequence1: Sequence,
+    sequence2: Sequence,
     binary_predicate: BinaryPredicate = operator.eq,
-) -> Optional[Tuple[Any, Any]]:
-    """
-    Find the first pair of elements from both iterables, that are considered not equal
-    Only indices until the last one of the shortest iterable are compared
+) -> Optional[Tuple[Any, Any]]:  # noqa E1136
+    """Find the first pair of elements from both sequences, that are considered not equal.
+
+    Only indices until the last one of the shortest sequence are compared.
 
     Args:
-        iterable1: First iterable to use for comparison
-        iterable2: Second iterable to use for comparison
-        binary_predicate: A binary predicate, that returns true if the elements from both iterables are considered equal
+        sequence1: First sequence to use for comparison
+        sequence2: Second sequence to use for comparison
+        binary_predicate: A binary predicate, that returns true if the elements from both sequences are considered equal
             If not provided, operator.eq will be used as a binary_predicate
 
     Returns:
-        None, if one or more iterables is empty, or they do not differ until the end of the shortest iterable.
-            If one indice has elements that are not considered equal, a Tuple of those elements will be returned
+        None, if one or more sequences is empty, or they do not differ until the end of the shortest sequence.
+            If one index has elements that are not considered equal, a Tuple of those elements will be returned
 
     Example:
-        mismatch([], []) returns None since both iterables are empty
+        mismatch([], []) returns None since both sequences are empty
 
-        mismatch([1, 2, 3, 4, 5], []) returns None, since one iterable is empty
+        mismatch([1, 2, 3, 4, 5], []) returns None, since one sequence is empty
 
-        mismatch([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]) returns None, since both iterables are considered equal
+        mismatch([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]) returns None, since both sequences are considered equal
 
-        mismatch([1, 2, 3, 4, 5], [1, 2, 3, 4, 6]) returns (5, 6), since  their fifth indice is considered not equal
+        mismatch([1, 2, 3, 4, 5], [1, 2, 3, 4, 6]) returns (5, 6), since  their fifth index is considered not equal
 
-        mismatch([1, 2, 3, 4, 5], [1, 2, 3, 4, 6, 7, 8, 9]) returns (5, 6), since  their fifth indice is considered not equal.
-        Since one iterable is longer, it's additional elements are not compared
+        mismatch([1, 2, 3, 4, 5], [1, 2, 3, 4, 6, 7, 8, 9]) returns (5, 6), since  their fifth index is considered not equal.
+        Since one sequence is longer, it's additional elements are not compared
     """
-    if not iterable1 or not iterable2:
+    if not sequence1 or not sequence2:
         return None
-    # Since this library implements a function named min, the builtin has to be accesses through another name
-    builtin_min: Callable = __builtins__["min"]
-    last_index_shortest_iterable: int = builtin_min(len(iterable1), len(iterable2)) - 1
+    last_index_shortest_sequence: int = min(len(sequence1), len(sequence2)) - 1
 
     return next(
-        (pair for pair in zip(iterable1, iterable2) if not binary_predicate(*pair)),
+        (pair for pair in zip(sequence1, sequence2) if not binary_predicate(*pair)),
         (
-            iterable1[last_index_shortest_iterable],
-            iterable2[last_index_shortest_iterable],
+            sequence1[last_index_shortest_sequence],
+            sequence2[last_index_shortest_sequence],
         ),
     )
 
 
 def find(collection: Collection, target_element: Any) -> int:
-    """
-        Find the index of the first occurrence of target_element in collection
+    """Find the index of the first occurrence of target_element in collection.
+
     Args:
         collection: A collection which to search through
         target_element: An element to search in the collection
@@ -247,8 +240,8 @@ def find(collection: Collection, target_element: Any) -> int:
 
 
 def find_if(collection: Collection, unary_predicate: UnaryPredicate) -> int:
-    """
-        Find the index of the first element in collection satisfying unary_predicate
+    """Find the index of the first element in collection satisfying unary_predicate.
+
     Args:
         collection: A collection which to search through
         unary_predicate: An UnaryPredicate, which determines if the current value is our target
@@ -263,8 +256,8 @@ def find_if(collection: Collection, unary_predicate: UnaryPredicate) -> int:
 
 
 def find_if_not(collection: Collection, unary_predicate: UnaryPredicate) -> int:
-    """
-        Find the index of the first element in collection NOT satisfying unary_predicate
+    """Find the index of the first element in collection NOT satisfying unary_predicate.
+
     Args:
         collection: A collection which to search through
         unary_predicate: An UnaryPredicate, which determines if the current value is NOT our target
@@ -283,8 +276,8 @@ def find_end(
     collection_sub: Collection,
     binary_predicate: BinaryPredicate = operator.eq,
 ) -> int:
-    """
-    Find index of the beginning of the last occurrence of collection_sub in collection_super
+    """Find index of the beginning of the last occurrence of collection_sub in collection_super.
+
     Args:
         collection_super: A Collection in which to search for the collection_sub
         collection_sub: A Collection to search for in collection_super
@@ -295,9 +288,11 @@ def find_end(
             or the last index in collection_super if it is empty or collection_sub is not found in it
 
     """
-    for i in range((len(collection_super) - 1) - (len(collection_sub) - 1), -1, -1):
+    for i in range(  # noqa: VNE001
+        (len(collection_super) - 1) - (len(collection_sub) - 1), -1, -1
+    ):
         if binary_predicate(
-                collection_super[i: i + len(collection_sub)], collection_sub
+            collection_super[i : i + len(collection_sub)], collection_sub  # noqa E203
         ):
             return i
 
@@ -305,12 +300,12 @@ def find_end(
 
 
 def find_first_of(
-        values_in: Collection,
-        values_from: Collection,
-        binary_predicate: BinaryPredicate = operator.eq,
+    values_in: Collection,
+    values_from: Collection,
+    binary_predicate: BinaryPredicate = operator.eq,
 ) -> int:
-    """
-    Find first index in values_in at which an element of values_from occurs
+    """Find first index in values_in at which an element of values_from occurs.
+
     Args:
         values_in: A Collection in which to search for values of values_from
         values_from: A Collection of values to search for in values_in
@@ -321,7 +316,7 @@ def find_first_of(
             or the last index in  values_in if it is empty or no element from values_from is found in it
 
     """
-    for i, element_in in enumerate(values_in):
+    for i, element_in in enumerate(values_in):  # noqa: VNE001
         for element_from in values_from:
             if binary_predicate(element_in, element_from):
                 return i
@@ -330,87 +325,89 @@ def find_first_of(
 
 
 def adjacent_find(
-    collection: Collection, binary_predicate: BinaryPredicate = operator.eq
+    sequence: Sequence, binary_predicate: BinaryPredicate = operator.eq
 ) -> int:
-    """
+    """Find the first index at which two adjacent elements are considered equal.
 
     Args:
-        collection: a collection to search through
+        sequence: a sequence to search through
         binary_predicate: a binary predicate to evaluate the equality of adjacent elements
 
     Returns:
+        The first index at which two adjacent elements are considered equal,
+            or the last index, if no two adjacent elements are considered equal,
+            or -1 if the sequence is empty
 
     """
-    if not collection:
+    if not sequence:
         return -1
     try:
+        # Multiplying index by two, since the list resulting from the call to map has
+        # half the length of the input, since it is sliced in half
         return (
-                list(map(binary_predicate, collection[::2], collection[1::2])).index(True)
-                * 2
+            list(map(binary_predicate, sequence[::2], sequence[1::2])).index(True) * 2
         )
     except ValueError:
-        return len(collection) - 1
+        return len(sequence) - 1
 
 
 def search(
-        collection_super: Collection,
-        collection_sub: Collection,
-        binary_predicate: BinaryPredicate = operator.eq,
+    sequence_super: Sequence,
+    sequence_sub: Sequence,
+    binary_predicate: BinaryPredicate = operator.eq,
 ) -> int:
-    """
-        Search for the first occurrence of collection_sub in collection_super
+    """Search for the first occurrence of sequence_sub in sequence_super.
+
     Args:
-        collection_super: A collection to search in
-        collection_sub: A collection to search for in collection_super
+        sequence_super: A sequence to search in
+        sequence_sub: A sequence to search for in sequence_super
         binary_predicate: A binary predicate to evaluate the equality of compared items
 
     Returns:
-        -The index of the beginning of the first occurrence of collection_sub in collection_super
-            -If not found, returns the last index of collection_super
-            -If any collection is empty, returns -1
+        The index of the beginning of the first occurrence of sequence_sub in sequence_super
+            If not found, returns the last index of sequence_super
+            If any sequence is empty, returns -1
     """
-
-    if not collection_sub:
+    if not sequence_sub:
         return -1
 
-    for i, element_super in enumerate(collection_super):
-        for element_super, element_sub in zip(collection_super[i:], collection_sub):
+    for i in range(len((sequence_super))):  # noqa: VNE001
+        for element_super, element_sub in zip(sequence_super[i:], sequence_sub):
             if not binary_predicate(element_super, element_sub):
                 break
-            else:
-                return i
 
-    return len(collection_super) - 1
+            return i
+
+    return len(sequence_super) - 1
 
 
 def search_n(
-        collection: Collection,
-        value: Any,
-        n: int,
-        binary_predicate: BinaryPredicate = operator.eq,
-):
-    """
-        Search for the first occurrence of n repetitions of value in collection
+    sequence: Sequence,
+    value: Any,  # noqa: VNE002
+    num_elements: int,  # noqa: VNE001
+    binary_predicate: BinaryPredicate = operator.eq,
+) -> int:
+    """Search for the first occurrence of num_elements repetitions of value in sequence.
+
     Args:
-        collection: A collection to search in
-        value: any object to search for in colletcion
-        n: How many times, counting from the beginning, value has to be repeated
+        sequence: A sequence to search in
+        value: any object to search for in collection
+        num_elements: How many times, counting from the beginning, value has to be repeated
         binary_predicate: A binary predicate to evaluate the equality of consecutive items
 
     Returns:
-        -The index of the beginning of the first n repititions of value in collection
-            -If not found, returns the last index of collection_super
-            -If collection is empty, returns -1
+        The index of the beginning of the first num_elements repetitions of value in sequence
+            If not found, returns the last index of sequence_super
+            If sequence is empty, returns -1
     """
-
-    if not collection:
+    if not sequence:
         return -1
 
-    for i, element in enumerate(collection):
-        for element in collection[i:]:
+    for i in range(len((sequence))):  # noqa: VNE001
+        for element in sequence[i : i + num_elements]:  # noqa: E203
             if not binary_predicate(element, value):
                 break
-            else:
-                return i
+        else:
+            return i
 
-    return len(collection) - 1
+    return len(sequence) - 1
